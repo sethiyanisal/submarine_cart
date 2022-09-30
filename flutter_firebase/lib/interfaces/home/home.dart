24a875f92617bfdf185980a9aff8657047f9e8b4
sethiyanisal <sethiya1098@gmail.com>
2022-09-30 20:25:07 +0530

    Created the sign out function

--- flutter_firebase/lib/interfaces/home/home.dart
+++ flutter_firebase/lib/interfaces/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/interfaces/settings/settings.dart';
 
 const List<String> list = <String>['One'];
 
@@ -24,15 +25,38 @@ class HomePage extends StatelessWidget {
           ),
         ),
         title: const Text('Submarine Cart'),
-        actions: <Widget>[
-          IconButton(
+        actions: [
+          PopupMenuButton<int>(
+            onSelected: (item) => onSelected(context, item),
             icon: const Icon(Icons.account_circle_outlined),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<int>(
+                value: 0,
+                child: Text('Settings'),
+              ),
+              const PopupMenuItem<int>(
+                value: 1,
+                child: Text('Sign Out'),
+              ),
+            ],
           ),
         ],
       ),
     );
+  }
+}
+
+void onSelected(BuildContext context, int item) {
+  switch (item) {
+    case 0:
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const SettingsPage(),
+        ),
+      );
+      break;
+    case 1:
+      FirebaseAuth.instance.signOut();
+      break;
   }
 }
